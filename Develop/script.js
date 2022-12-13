f0b8d39b82d2393cba4ccdc5114b71530004cee3
DuretiTilmo <112039625+DuretiTilmo@users.noreply.github.com>
2022-12-12 20:02:41 -0800

    Description and a GIF added to the README file

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var today = dayjs();
 
-
-var today = moment().format('dddd, MMM Do');
-$("#currentDay").text(today);
-
+$('#currentDay').text(today.format('dddd, MMMM DD')); 
 
 // Add Event Listener to the button
 $(".saveBtn").on("click", function() {
@@ -14,15 +9,16 @@ var time = $(this).parent().attr("id");
 
 // saving event to local storage
 localStorage.setItem(time, input);
+
 });
 
 function displayTime() { 
-   
-  var rightNow = moment().hour();
+
+  var rightNow = dayjs().hour();
 
       $(".time-block").each(function () {
             var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-  
+
             if (timeBlock < rightNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
@@ -51,25 +47,7 @@ function displayTime() {
      $("#hour14 .description").val(localStorage.getItem("hour14"));
      $("#hour15 .description").val(localStorage.getItem("hour15"));
      $("#hour16 .description").val(localStorage.getItem("hour16"));
-     $("#hour17 .description").val(localStorage.getItem("hour17"));
+     $("#hour17 .description").val(localStorage.getItem("hour17"));   
 
   displayTime();
 
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Module-5-Challenge
-Third-Party APIs Challenge
+
+## Third-Party APIs Challenge: Work Day Scheduler
+
+## Description
+
+This application is a work day scheduler/simple calender application/ that allows user to save events for each hour of the day. Based on the provided starter code Day.js library , jquery and different Javascript solutions are utilized to make this page functional. 
+
+URL: 
+
+![A user adds and saves events on the color coded calender having different colors for past, present and future times](Assets/page-made-functional.gif)
+
+-----
+&copy; 2022 
