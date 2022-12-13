2297d139cff0fcec7bc0f0104c83c9561125e317
DuretiTilmo <112039625+DuretiTilmo@users.noreply.github.com>
2022-12-12 18:16:20 -0800

    added functioality to the page using Day.js library and jQuery

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,9 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-var currentTimeEl = $('.present');
-var pastTimeEl = document.querySelector('.past');
-var futureTimeEl = document.querySelector('.future');
-var saveBtn = document.querySelector('.saveBtn');
-var textEl = document.querySelector('.description');
-var blockTimeEl = $('.time-block');
 
-var today = dayjs();
-
-$('#currentDay').text(today.format('dddd, MMMM DD')); 
+var today = moment().format('dddd, MMM Do');
+$("#currentDay").text(today);
 
 
 // Add Event Listener to the button
@@ -21,8 +14,46 @@ var time = $(this).parent().attr("id");
 
 // saving event to local storage
 localStorage.setItem(time, input);
-})
+});
 
+function displayTime() { 
+   
+  var rightNow = moment().hour();
+
+      $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+  
+            if (timeBlock < rightNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === rightNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+    
+            }
+        });
+  }
+
+     // Get user's input that was saved in localStorage
+     $("#hour9 .description").val(localStorage.getItem("hour9"));
+     $("#hour10 .description").val(localStorage.getItem("hour10"));
+     $("#hour11 .description").val(localStorage.getItem("hour11"));
+     $("#hour12 .description").val(localStorage.getItem("hour12"));
+     $("#hour13 .description").val(localStorage.getItem("hour13"));
+     $("#hour14 .description").val(localStorage.getItem("hour14"));
+     $("#hour15 .description").val(localStorage.getItem("hour15"));
+     $("#hour16 .description").val(localStorage.getItem("hour16"));
+     $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+  displayTime();
 
 // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
